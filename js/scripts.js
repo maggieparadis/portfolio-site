266f0f6f29d9ec59927e540944b02b0ffac1bba1
maggieparadis <maggieparadis@gmail.com>
2021-01-19 14:10:05 -0500

    create mobile navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Welcome to Maggie's Portfolio Site");
+function navtoggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
